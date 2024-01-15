be7aa22ac569530fb3f3fd05a42fbbc42772123b
Jake Willmsen <jdwillmsen@gmail.com>
2024-01-15 17:43:01 -0600

    feat: Add go-next-ts-chat project
    This contains a server and client setup with the server containing all the backend code and the client containing all the frontend code. The server code is written in go and the client code is written in typescript with next js.

---
+++ go-next-ts-chat/README.md
@@ -0,0 +1,2 @@
+# Go Next TypeScript Chat Project
+A realtime chat application written with Go, Next, TypeScript, and Postgres.
---
+++ go-next-ts-chat/server/internal/router/router.go
@@ -0,0 +1,40 @@
+package router
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/jdwillmsen/go-chat-server/internal/user"
+	"github.com/jdwillmsen/go-chat-server/internal/ws"
+	"time"
+)
+
+var r *gin.Engine
+
+func InitRouter(userHandler *user.Handler, wsHandler *ws.Handler) {
+	r = gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "http://localhost:3000"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
+	r.POST("/signup", userHandler.CreateUser)
+	r.POST("/login", userHandler.Login)
+	r.GET("/logout", userHandler.Logout)
+
+	r.POST("/ws/create-room", wsHandler.CreateRoom)
+	r.GET("/ws/join-room/:roomId", wsHandler.JoinRoom)
+	r.GET("/ws/get-rooms", wsHandler.GetRooms)
+	r.GET("/ws/get-clients/:roomId", wsHandler.GetClients)
+}
+
+func Start(addr string) error {
+	return r.Run(addr)
+}
